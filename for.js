f0681c4f06661671a5c87333100f1aef3fc6df91
deluardon <160032360+deluardon@users.noreply.github.com>
2024-05-20 00:31:25 +0600

    fast commit

---
+++ for.js
@@ -0,0 +1,129 @@
+// for (let i = 0; i < 5; i++) {
+//     // console.log(i);
+// }
+
+// function add (y, x){
+//      const result = y + x;
+//      return result;
+   
+
+// }
+// add(9,56)
+
+
+// const fruits = ["mango","tango","congo","bang"];
+
+// fruits.map((item,index,arr) =>{
+//     // console.log(item,index,arr);
+// })
+
+// const array =[1,2,3,4,5,6,7,8,9,10,11]
+
+//     const sum = array.reduce ((previous,current) => current + previous)
+    // console.log(sum);
+
+// previous + current = result
+// 0 + 1 = 1;
+// 1 + 2 = 3;
+// 3 + 4 = 7;
+// 7 + 5 = 12;
+// 12 + 6 = 18
+// 18 + 7 = 25
+// 25 + 8 = 35
+// 44 + 10 = 54
+// 54 + 11 = 66
+
+
+
+
+// const number =[8,5,4,3,2,6,8,5,];
+
+// const a =(x) => {
+//     console.log(x);
+// }
+
+// number.forEach(a);
+
+// const f = ["mango","tango","kal" ,"dal","cal"];
+
+// const y =(item => {
+//     console.log(item);
+// })
+    
+
+
+// f.forEach(y);
+
+
+// const number =[1,2,3,4];
+// let sum=0;
+// number.forEach(item => sum += item);
+// console.log(item,);
+
+
+// item(2)
+
+// const number = [1,2,3,4];
+// let sum = 0;
+
+// number.forEach(item => {
+//     sum += item;
+// });
+
+// console.log(sum);  // Corrected to log the sum, which is the intended output.
+
+// const number = [1, 2, 3, 4];
+// let sum = 0;
+
+// number.forEach(item => {
+//     sum += item;
+//     console.log(item);  // Logs each item
+// });
+
+// console.log(sum);  // Logs 10
+
+
+
+// const number =[1,2,3,4,5,6,7,8,9]
+//  const filter = number => {
+//     const result=[]
+//     if(item % 2 === 0){
+//         result.push(item);
+//     }
+//     return result;
+//  }
+//  const x = filter(number)
+//  console.log(x);
+
+//  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const filter = (numbers) => {
+//     const result = [];
+//     numbers.forEach(item => {
+//         if (item % 2 === 0) {
+//             result.push(item);
+//         }
+//     });
+//     return result;
+// }
+
+// const x = filter(numbers);
+// console.log(x);  // Output: [2, 4, 6, 8]
+
+
+//  const numbers = [1, 3 , 5 , 7, 9];
+
+// const filter = (numbers) => {
+//     const result = [];
+//     numbers.forEach(item => {
+//         if (item % 2 === 0) {
+//             result.push(item);
+//         }
+//     });
+//     return result;
+// }
+
+// const x = filter(numbers);
+// console.log(x); 
+
+
